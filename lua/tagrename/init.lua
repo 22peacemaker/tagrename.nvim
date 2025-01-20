75e5d6bb4f33b036722a59c8d157408b8d68e31b
Jannis Karpinski <jkarpinski@omoti.de>
2025-01-20 13:18:18 +0100

    Examples

---
+++ lua/tagrename/init.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.setup(opts)
+	opts = opts or {}
+	vim.keymap.set("n", "<leader>hh", function()
+		if opts.name then
+			print("Hello, " .. opts.name)
+		else
+			print("Hello, world!")
+		end
+	end)
+end
